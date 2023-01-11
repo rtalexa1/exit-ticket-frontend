a8529dedc0a394fef9ea8129ec869e3a18238294
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-11 12:24:16 -0600

    Remove default content and create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <NavBar />
 </template>
 
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+html {
+  overflow-y: scroll;
+  height: 100%;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+
+  background-color: #f2f2f2;
+  // color: var(--color-text-body);
+  line-height: 1.5;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav-bar">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.nav-bar {
+  display: flex;
+  position: fixed;
+  width: 100%;
+  background-color: #253c55;
+}
+</style>
