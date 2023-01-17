19c70656a866a0fd3d7ccbff9546500e8eedfdcf
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-01-17 15:02:53 -0600

    Set up store with currentTicket and exitTickets state, as well as addExitTicket mutation and addExitTicket action

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    currentTicket: "",
+    exitTickets: [],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    addExitTicket(state, newExitTicket) {
+      state.exitTickets.push(newExitTicket);
+    },
+  },
+  actions: {
+    async addExitTicket({ commit }, exitTicketId) {
+      const res = await fetch(
+        `http://localhost:3000/users/1/exit_tickets/${exitTicketId}`
+      );
+      const data = await res.json();
+      commit("addExitTicket", data);
+    },
+  },
   modules: {},
 });
