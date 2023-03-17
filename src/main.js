332a07b0648119394dfd32ab2d8c506981e3037f
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-03-16 21:08:01 -0500

    Delete all Firebase code from main.js

--- src/main.js
+++ src/main.js
@@ -2,27 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// eslint-disable-next-line no-unused-vars
-import firebase from "firebase/compat/app";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faBars, faPlus, faX } from "@fortawesome/free-solid-svg-icons";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBPwsfdGzQ73nUuLkViabUD-RpAzmXmOmU",
-  authDomain: "exit-ticket-d0384.firebaseapp.com",
-  projectId: "exit-ticket-d0384",
-  storageBucket: "exit-ticket-d0384.appspot.com",
-  messagingSenderId: "422240912790",
-  appId: "1:422240912790:web:8eb6462e0db21a6db7c9b7",
-  measurementId: "G-RKGPC9QLN2",
-};
-
-const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
-const analytics = getAnalytics(app);
 
 library.add(faBars, faPlus, faX);
 
